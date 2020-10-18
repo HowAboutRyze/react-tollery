861c1fb9516b8ff3fac77454ec365720c27537cf
HowAboutRyze <lizhongwei157@163.com>
2020-10-18 23:43:24 +0800

    deploy

---
+++ precache-manifest.92e3ba955e60fb70c0ea50685ff8f821.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9801d97d36ed5b5af8038164e46ce415",
+    "url": "./index.html"
+  },
+  {
+    "revision": "74c4072fd4aa8a7be439",
+    "url": "./static/css/main.bc8d8336.chunk.css"
+  },
+  {
+    "revision": "fcec78c588bafd3dee59",
+    "url": "./static/js/2.de8d4e07.chunk.js"
+  },
+  {
+    "revision": "74c4072fd4aa8a7be439",
+    "url": "./static/js/main.17b350b2.chunk.js"
+  },
+  {
+    "revision": "dc2241f3ecd667e9ee1d",
+    "url": "./static/js/runtime~main.b09a19b8.js"
+  }
+]);
